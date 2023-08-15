a6862c889690e744a08ca2aeeb58a843a8a5b89d
DaniellePinheiro <daniellepinheirosilva123@hotmail.com>
2023-08-15 19:36:11 -0300

    versionamento

--- bucket.tf
+++ bucket.tf
@@ -1,8 +1,12 @@
 resource "aws_s3_bucket" "projeto_terraform_bucket" {
   bucket = "projeto-terraform2112"
-  versioning {
-    enabled = false
-  }
 
   tags = local.common_tags
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.projeto_terraform_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
 }
